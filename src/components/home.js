f8bda0ee3290a3719c453028a84463c971945731
A.D <adrianlebaron@gmail.com>
2018-11-30 10:49:10 -0700

    had to re start my madlib file should push every day

--- src/components/home.js
+++ src/components/home.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
- import Header from './header';
- import Card from './card';
+import Header from './header';
+import Card from './card';
 
- class Home extends Component {
-   render() {
-     return (
-       <div className="home">
-         { Header() }
-         <Card />
-       </div>
-     );
-   }
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        { Header() }
+        <Card />
+      </div>
+    );
+  }
 }
 
 export default Home;
